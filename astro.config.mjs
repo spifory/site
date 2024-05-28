03c150a30b48082f0aeec5b512fef0122b81580e
spifory <spifory@protonmail.com>
2024-05-28 20:07:40 +0200

    feat: add astro with config

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [icon({
+        include: {
+            twemoji: ["*"],
+            "skill-icons": ["*"],
+            mdi: ["*"]
+        }
+    })]
+});
