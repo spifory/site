efda67534e328eff7da363793eb60eea4dc8a339
spifory <spifory@protonmail.com>
2024-06-14 18:16:29 +0200

    fix(blog): hardcode domain

--- astro.config.ts
+++ astro.config.ts
@@ -4,11 +4,12 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://spf.lol',
     integrations: [icon({
         include: {
             twemoji: ["*"],
             "skill-icons": ["*"],
             mdi: ["*"]
         }
-    })]
+    })],
 });
